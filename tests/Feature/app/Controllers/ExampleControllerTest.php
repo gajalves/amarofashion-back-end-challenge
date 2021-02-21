bf25d47779c59b8678519c61a196eaee427efdce
joimo <ga.jaimealves@gmail.com>
2021-02-21 15:27:07 -0300

    wip

---
+++ tests/Feature/app/Controllers/ExampleControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\App\Controllers;
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+use TestCase;
+
+class ExampleControllerTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->get('/');
+
+        $this->assertEquals(
+            $this->app->version(), $this->response->getContent()
+        );
+    }
+}
